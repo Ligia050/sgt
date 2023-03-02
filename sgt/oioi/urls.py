04f13f69291fb9013cdb4109e9ed198bddd6aef9
Ligia050 <ligia.castela05@aluno.ifce.edu.br>
2023-03-02 16:59:10 -0300

    bom dia

--- sgt/oioi/urls.py
+++ sgt/oioi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ path("", views.index, name = "secundario"),
-		path("<str:nome>", views.saudacao,name="saudacao")]
+urlpatterns = [ 
+		#path("<str:nome>", views.greet,name="greet"),
+		path("<str:nome>", views.tia_do_zap,name="tia do zap")
+		]
--- sgt/oioi/views.py
+++ sgt/oioi/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
 	return HttpResponse("Oi pessoal, criei agora essa nova aplicação que bonitinha ;) ")
 
 def saudacao(request,nome):
 	return render(request,'index.html')
+
+def greet(request,nome):
+	return render(request,'greet.html',{'nome':nome})
+
+def tia_do_zap(request,nome):
+	return render(request,'tia_do_zap.html',{'nome':nome,'dia':False})
